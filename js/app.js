83dd7edaeaa2cb127727cabe0051a872f8454acf
vrMars <vrmars98@gmail.com>
2017-09-04 20:19:56 -0400

    bg fixxed

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 10,
+        "value": 7,
         "density": {
           "enable": true,
           "value_area": 100
